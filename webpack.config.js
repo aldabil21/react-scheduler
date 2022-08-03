23dacd9a1406f307b698f6ee3e526fe2cf5aa593
aldabil21 <ali.aldabil@gmail.com>
2022-08-03 14:08:17 +0300

    Remove css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,6 @@ module.exports = {
         exclude: /node_modules/,
         use: ["ts-loader"],
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
     ],
   },
   resolve: {
