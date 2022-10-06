c3df943f2259aaac445858b49dc6bf2c2125cb1d
Abdulraoof <ali.aldabil@gmail.com>
2022-10-06 19:10:02 +0300

    Adjust up events dates

--- src/model/events.ts
+++ src/model/events.ts
@@ -1,81 +1,59 @@
-export const EVENTS = [
-  // {
-  //   event_id: 1,
-  //   title: "Event 1",
-  //   start: new Date("2021 5 2 10:30"),
-  //   end: new Date("2021 5 2 12:30"),
-  //   admin_id: [1, 2, 3, 4],
-  //   disabled: true,
-  // },
-  // {
-  //   event_id: 2,
-  //   title: "Event 2",
-  //   start: new Date("2021 5 4 10:00"),
-  //   end: new Date("2021 5 4 11:00"),
-  //   admin_id: 2,
-  // },
-  // {
-  //   event_id: 3,
-  //   title: "Event 3",
-  //   start: new Date("2021 4 27 09:00"),
-  //   end: new Date("2021 4 28 10:00"),
-  //   admin_id: 1,
-  // },
-  // {
-  //   event_id: 4,
-  //   title: "Event 4",
-  //   start: new Date("2021 5 4 9:00"),
-  //   end: new Date("2021 5 4 10:36"),
-  //   admin_id: 2,
-  // },
-  // {
-  //   event_id: 5,
-  //   title: "Event 5",
-  //   start: new Date("2021 5 1 10:00"),
-  //   end: new Date("2021 5 18 11:00"),
-  //   admin_id: 2,
-  // },
-  // {
-  //   event_id: 6,
-  //   title: "Event 6",
-  //   start: new Date("2021 5 2 12:00"),
-  //   end: new Date("2021 5 2 13:00"),
-  //   admin_id: 2,
-  // },
-  {
-    event_id: 7,
-    title: "Event 7",
-    start: new Date("2021/5/4 09:00"),
-    end: new Date("2021/5/5 09:20"),
-    admin_id: [3],
-  },
-  {
-    event_id: 8,
-    title: "Event 8",
-    start: new Date("2021/5/1 13:00"),
-    end: new Date("2021/5/16 14:00"),
-    admin_id: [4, 3],
-  },
-  {
-    event_id: 9,
-    title: "Event 9",
-    start: new Date("2021/5/1 10:00"),
-    end: new Date("2021/5/3 11:00"),
-    admin_id: [4],
-  },
+import type { ProcessedEvent } from "../lib/types";
 
-  // {
-  //   event_id: 10,
-  //   title: "Event 9",
-  //   start: new Date("2021 5 6  15:00"),
-  //   end: new Date("2021 5 6 16:00"),
-  //   admin_id: 2,
-  // },
-  // {
-  //   event_id: 11,
-  //   title: "Event 10",
-  //   start: new Date("2021 5 6 14:00"),
-  //   end: new Date("2021 5 6 15:00"),
-  //   admin_id: 2,
-  // },
+export const EVENTS: ProcessedEvent[] = [
+  {
+    event_id: 1,
+    title: "Event 1",
+    start: new Date(new Date(new Date().setHours(9)).setMinutes(0)),
+    end: new Date(new Date(new Date().setHours(10)).setMinutes(0)),
+    disabled: true,
+    admin_id: [1, 2, 3, 4],
+  },
+  {
+    event_id: 2,
+    title: "Event 2",
+    start: new Date(new Date(new Date().setHours(10)).setMinutes(0)),
+    end: new Date(new Date(new Date().setHours(12)).setMinutes(0)),
+    admin_id: 2,
+    color: "#50b500",
+  },
+  {
+    event_id: 3,
+    title: "Event 3",
+    start: new Date(new Date(new Date().setHours(11)).setMinutes(0)),
+    end: new Date(new Date(new Date().setHours(12)).setMinutes(0)),
+    admin_id: 1,
+  },
+  {
+    event_id: 4,
+    title: "Event 4",
+    start: new Date(
+      new Date(new Date(new Date().setHours(9)).setMinutes(30)).setDate(new Date().getDate() - 2)
+    ),
+    end: new Date(
+      new Date(new Date(new Date().setHours(11)).setMinutes(0)).setDate(new Date().getDate() - 2)
+    ),
+    admin_id: 2,
+    color: "#900000",
+  },
+  {
+    event_id: 5,
+    title: "Event 5",
+    start: new Date(
+      new Date(new Date(new Date().setHours(10)).setMinutes(30)).setDate(new Date().getDate() - 2)
+    ),
+    end: new Date(
+      new Date(new Date(new Date().setHours(14)).setMinutes(0)).setDate(new Date().getDate() - 2)
+    ),
+    admin_id: 2,
+  },
+  {
+    event_id: 5,
+    title: "Event 5",
+    start: new Date(
+      new Date(new Date(new Date().setHours(10)).setMinutes(30)).setDate(new Date().getDate() - 4)
+    ),
+    end: new Date(new Date(new Date().setHours(14)).setMinutes(0)),
+    admin_id: 2,
+  },
 ];
