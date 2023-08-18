990ac06353fd26c93582742691e6ded38029f3f9
ATonikyan <sash.tonikyan@gmail.com>
2023-08-18 16:41:05 +0400

    add tgz file for npm

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,20 @@ function App() {
     <Scheduler
       ref={calendarRef}
       events={EVENTS}
+      week={{
+        weekDays: [0, 1, 2, 3, 4, 5, 6],
+        weekStartOn: 1,
+        startHour: 9,
+        endHour: 14,
+        step: 60,
+        timeRanges: [
+          { label: "1-2", value: 9 },
+          { label: "3-4", value: 11 },
+          { label: "5-6", value: 13 },
+          { label: "7", value: 15 },
+          { label: "8", value: 16 },
+        ],
+      }}
       // events={generateRandomEvents(200)}
     />
   );
